8be2a8374312a614819cfc010d61119fb40a3cee
babujal <dercamar1@gmail.com>
2024-09-18 11:30:31 -0400

    completed

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const car = {
 };
 
 // Your code here
-const {make, model} = car;
+const { make, model } = car;
 
 console.log('Obj Destructuring----------------------------');
 console.log(make);
@@ -67,12 +67,12 @@ console.log(controversialPizzaToppings);
 //     make: 'Audi',  Commented cause this array exist already on line 36!
 //     model: 'q5',
 // };
-  
+
 // Change the `model` property of `myCar` to 'q7'. Log both objects.
-  
+
 // Your code here
 console.log('Spread Operator on Objects------------------------------');
-const myCar = {...car};
+const myCar = { ...car };
 myCar.model = 'q7';
 console.log(car);
 console.log(myCar);
@@ -121,12 +121,62 @@ sentence()
 let pizza = 'tasty';
 
 if (pizza === 'tasty') {
-  console.log('yum');
+    console.log('yum');
 } else {
-  console.log('yuck');
+    console.log('yuck');
 }
 
 // Your code here
+console.log('The ternary operator------------------------------');
 let tester;
 const exerciseNine = pizza === 'tasty' ? tester = 'yum' : tester = 'yuck';
 console.log(`The pizza is ${tester}`)
+
+/////////////////////////////////////////////////\
+
+// Exercise 10: Boolean gates
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+// Log the result
+console.log('Boolean gates || (or) operator------------------------------');
+const LANG = localLangConfig || 'en'
+console.log('Language setting:', LANG);
+///////////
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || 'light';
+// Log the result
+console.log('User theme setting:', USER_THEME);
+
+/////////////////////////////////////////////////\
+
+// Exercise 11: Optional chaining
+
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+    name: 'Alice',
+};
+
+let cat = adventurer?.cat?.age; // Your code here
+
+console.log(cat);
